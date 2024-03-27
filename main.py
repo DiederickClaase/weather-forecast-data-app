3054e96cf17a0868309d06234edcba0ab49228d2
diederick <diederickc@gmail.com>
2024-03-27 20:59:17 +0200

    Day33 import images, load weather and image that match the town. Try except if no match city

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from backend import get_data
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the number of Days")
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+option = st.selectbox("Select data to view", ("Temperature", "Sky Conditions"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
 
+    try:
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10. for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-
-
+        if option == "Sky Conditions":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png",
+                      "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=85)
+    except KeyError:
+        st.error("Place does not seem to exist")
